5044e750f0b43427c38a5878652fee4c8879ceba
JaeCheol Eom <abctmxk@naver.com>
2022-03-07 14:28:32 +0900

    ttetetet

--- src/testtest.java
+++ src/testtest.java
@@ -3,15 +3,18 @@ public class testtest {
 
 	public static void main(String[] args) {
 		System.out.println("테스트");
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
 		System.out.println("home2branch");
-=======
+
 		System.out.println("브랜치 변경해보자");
->>>>>>> stash
-=======
+
+
 		System.out.println("브랜치 변경해보자");
->>>>>>> stash
+
+
+		System.out.println("브랜치 변경해보자");
+
 
 	}
 
