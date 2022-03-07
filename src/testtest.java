efedf4bfc7cc1aed757ec285e98dc31d6b2446ad
JaeCheol Eom <abctmxk@naver.com>
2022-03-07 14:18:32 +0900

    브랜치 변경됬어요. 아까 방금요

--- src/testtest.java
+++ src/testtest.java
@@ -3,7 +3,11 @@ public class testtest {
 
 	public static void main(String[] args) {
 		System.out.println("테스트");
+<<<<<<< HEAD
 		System.out.println("home2branch");
+=======
+		System.out.println("브랜치 변경해보자");
+>>>>>>> stash
 
 	}
 
